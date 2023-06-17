3176c15f1f0bb56ef763eeccef0e6b18778fe125
Yuchi Yamaguchi <abap0002@gmail.com>
2023-06-17 17:39:19 +0900

    全力revert

--- Q_Q/main.py
+++ Q_Q/main.py
@@ -1,10 +1,9 @@
+import os
+
 from traq_bot import TraqBot
 from traq.api import message_api
 import traq
-import os
-
-from channel import join_channel
-from massage import send_message
+from traq.model.post_message_request import PostMessageRequest
 
 CONFIGURATION = traq.Configuration(
     host = "https://q.trap.jp/api/v3"
@@ -14,19 +13,44 @@ CONFIGURATION.access_token = os.environ.get("ACCESS_TOKEN")
 
 bot = TraqBot(os.environ.get("VERIFY_TOKEN"))
 
+def subchannels(all_channels, configuration):
+    pass
+
+
+def join_channel(channel_id, configuration):
+    pass
+
+
+def leave_channel(channel_id, configuration):
+    pass
+
+def send_message(channel_id, message, configuration):
+    message_api.create_message(channel_id, message)
+    with traq.ApiClient(configuration) as api_client:
+        api_instance = message_api.MessageApi(api_client)
+        post_message_request = PostMessageRequest(
+            content=message,
+            embed=False,
+        ) 
+
+    try:
+        api_response = api_instance.post_message(channel_id, post_message_request=post_message_request)
+    except traq.ApiException as e:
+        print("Exception when calling MessageApi->post_message: %s\n" % e)
+
+    return api_response
+
 @bot.message_created
 def message_created(message):
-    print('get message')
     Q_Q_USER_ID = "c75f0d59-9722-416b-be31-b21375378690"
     for embedded in message["embedded"]:
         # 自分へのメンションを含むか判定
         if embedded.has_key("type") and embedded["type"] == "user" and  embedded["id"] == Q_Q_USER_ID:
-            print('this is mention')
-            print('join')
             join_channel(message["channelId"], CONFIGURATION)
-            print('send')
             send_message(message["channelId"], "Q_Q < :oisu:", CONFIGURATION)
     
+
+
 
 if __name__ == '__main__':
     print('Start bot...')
