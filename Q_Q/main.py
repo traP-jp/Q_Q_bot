9d1407f985a96a71d39034811c83b333a32bf3b7
Yuchi Yamaguchi <abap0002@gmail.com>
2023-06-17 15:56:11 +0900

    debug

--- Q_Q/main.py
+++ Q_Q/main.py
@@ -1,14 +1,4 @@
 from traq_bot import TraqBot
 import os
 
-
-bot = TraqBot(os.environ.get("ACCESS_TOKEN"))
-
-@bot.message_created
-def print_message_data(data: dict):
-    print(data)
-
-
-if __name__ == '__main__':
-    print('Start bot...')
-    bot.run(8080)
+print("Hello world")
