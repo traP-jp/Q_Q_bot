d32c1c331d9062e574139cd2e4a8649b6ac05975
Yuchi Yamaguchi <abap0002@gmail.com>
2023-06-17 17:08:43 +0900

    メンションへの反応を実装

---
+++ Q_Q/channel.py
@@ -0,0 +1,14 @@
+import traq 
+from traq.api import message_api, channel_api
+
+def subchannels(all_channels, configuration):
+    pass
+
+
+def join_channel(channel_id, configuration):
+    pass
+
+
+def leave_channel(channel_id, configuration):
+    pass
+
--- Q_Q/main.py
+++ Q_Q/main.py
@@ -1,13 +1,33 @@
 from traq_bot import TraqBot
+from traq.api import message_api
+import traq
 import os
 
+from channel import join_channel
+from massage import send_message
 
-bot = TraqBot(os.environ.get("ACCESS_TOKEN"))
+CONFIGURATION = traq.Configuration(
+    host = "https://q.trap.jp/api/v3"
+)
+
+CONFIGURATION.access_token = os.environ.get("ACCESS_TOKEN")
+
+
+
+
+bot = TraqBot(os.environ.get("VERIFY_TOKEN"))
+
+
 
 @bot.message_created
-def print_message_data(data: dict):
-    print(data)
-
+def message_created(message):
+    Q_Q_USER_ID = "c75f0d59-9722-416b-be31-b21375378690"
+    for embedded in message["embedded"]:
+        # 自分へのメンションを含むか判定
+        if embedded.has_key("type") and embedded["type"] == "user" and  embedded["id"] == Q_Q_USER_ID:
+            join_channel(message["channelId"], CONFIGURATION)
+            send_message(message["channelId"], "Q_Q < :oisu:", CONFIGURATION)
+    
 
 if __name__ == '__main__':
     print('Start bot...')
